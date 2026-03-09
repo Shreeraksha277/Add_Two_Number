27cd8c6ed396ba25623c7b650b90b1f0034e16a9
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-03-09 20:24:59 +0530

    Update add.java

--- add.java
+++ add.java
@@ -3,7 +3,7 @@
          int n=nums.length;
         for(int i=0;i<n;i++)
         {
-             for(int j=i+1;j<n;j++)
+              for(int j=i+1;j<n;j++)
              {
                 if(nums[i]+nums[j]==target)
         
