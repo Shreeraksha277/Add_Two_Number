d4d2e1af6a30770c22a3e06086f99ab0be476539
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-03-30 20:05:15 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,7 +2,7 @@
     public int[] twoSum(int[] nums, int target) {
          int n=nums.length;
         for(int i=0;i<n;i++)
-        {
+        { 
               for(int j=i+1;j<n;j++)
              {
                 if(nums[i]+nums[j]==target)
