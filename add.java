b27a0aa35a7e24540f1b0cc60467117acd2d4233
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-03-30 20:05:38 +0530

    Fix formatting issues in add.java

--- add.java
+++ add.java
@@ -4,7 +4,7 @@
          for(int i=0;i<n;i++)
         { 
               for(int j=i+1;j<n;j++)
-             {
+              {
                 if(nums[i]+nums[j]==target)
         
            return new int[] {i,j};
