f17c292365d47735bea335a1db21bab9a56e93f9
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-05-29 20:25:25 +0530

    Update hash.java

--- hash.java
+++ hash.java
@@ -7,7 +7,7 @@ class solution{
           int complement= target -nums[i];
         if(map.containsKey(complement))
         {
-          return new int[]{map.get(complement),i);
+           return new int[]{map.get(complement),i);
          }
         map.put(nums[i],i);
       }
