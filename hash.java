73337a13a1e168b142349a3318e303d56b9e7129
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-07-01 11:09:42 +0530

    Update hash.java

--- hash.java
+++ hash.java
@@ -3,7 +3,7 @@ class solution{
   { 
       HashMap<Integer,Integer> map = new HashMap<>();
     for(int i=0i<nums.length;i++)
-       {
+        {
           int complement= target -nums[i];
         if(map.containsKey(complement))
         {
