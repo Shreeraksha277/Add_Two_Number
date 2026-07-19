6356ff5f9cf87220f1a12bd51557faae587597af
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-07-19 09:24:29 +0530

    Update hash.java

--- hash.java
+++ hash.java
@@ -2,7 +2,7 @@ class solution{
       public static int twosum(int[] nums, int target)
   { 
       HashMap<Integer,Integer> map = new HashMap<>();
-    for(int i=0i<nums.length;i++)
+     for(int i=0i<nums.length;i++)
          {
           int complement= target -nums[i];
         if(map.containsKey(complement))
